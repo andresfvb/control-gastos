7b24a6de67a0698f3a3a1bb7913f91dccf4329f2
Andres Vasquez <barandicandres@gmail.com>
2023-07-17 15:02:53 -0500

    Finalizamos Control Gastos

---
+++ src/components/Modal.jsx
@@ -0,0 +1,106 @@
+import CerrarModalImg from '../img/cerrar.svg'
+import {useState, useEffect} from 'react'
+import Mensaje from './Mensaje'
+
+
+const Modal = ({setModal, animarModal, setAnimarModal, guardarGasto, gastoEditar, setGastoEditar}) => {
+
+    const [mensaje, setMensaje] = useState('')
+
+    const [nombre, setNombre] = useState('')
+    const [cantidad, setCantidad] = useState('')
+    const [categoria, setCategoria] = useState('')
+    const [id, setId] = useState('')
+    const [fecha, setFecha] = useState('')
+
+    useEffect(() =>{
+        if(Object.keys(gastoEditar).length > 0){
+            setNombre(gastoEditar.nombre)
+            setCantidad(gastoEditar.cantidad)
+            setCategoria(gastoEditar.categoria)
+            setId(gastoEditar.id)
+            setFecha(gastoEditar.fecha)
+        }
+
+    }, [])
+
+    const ocultarModal = () => {
+        setAnimarModal(false)
+        setGastoEditar({})
+        setTimeout(()=>{
+            setModal(false)
+        }, 500)
+    }
+
+    const handleSubmit = e =>{
+        e.preventDefault()
+        if([nombre, cantidad, categoria].includes('')){
+            setMensaje('Todos los campos son obligatorios')
+            setTimeout(()=>{
+                setMensaje('')
+            },2000)
+            return
+        }
+        guardarGasto({nombre, cantidad, categoria, id, fecha})
+    }
+    return (
+        <div className="modal">
+            <div className="cerrar-modal">
+                <img
+                    src={CerrarModalImg}
+                    alt="Cerrar modal"
+                    onClick={ocultarModal}
+                />
+            </div>
+            <form 
+                onSubmit={handleSubmit}
+                className={`formulario ${animarModal ? "animar" : "cerrar"}`}>
+                <legend>{gastoEditar.nombre ? "Editar Gasto" : 'Nuevo Gasto'}</legend>
+                {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
+                <div className='campo'>
+                    <label htmlFor="nombre">Nombre Gasto</label>
+                    <input
+                        id="nombre"
+                        type="text"
+                        placeholder='Añade el Nombre del gasto'
+                        value={nombre}
+                        onChange={e=>setNombre(e.target.value)}
+                    />
+                </div>
+                <div className='campo'>
+                    <label htmlFor="nombre">Cantidad</label>
+                    <input
+                        id="cantidad"
+                        type="text"
+                        placeholder='Añade la cantidad del gasto'
+                        value={cantidad}
+                        onChange={e => setCantidad(Number(e.target.value))}
+                    />
+                </div>
+                <div className='campo'>
+                    <label htmlFor="categria">Categoria</label>
+                    <select
+                        id="categoria"
+                        value={categoria}
+                        onChange={e => setCategoria(e.target.value)}
+                    >
+                        <option value="">-- Seleccione --</option>
+                        <option value="ahorro">Ahorro</option>
+                        <option value="comida">Comida</option>
+                        <option value="casa">Casa</option>
+                        <option value="gastos">Gastos Varios</option>
+                        <option value="salud">Salud</option>
+                        <option value="suscripciones">Suscripciones</option>
+                        <option value="ocio">Ocio</option>
+                    </select>
+                </div>
+                <input
+                    type="submit"
+                    value={gastoEditar.nombre ? "Actualizar" : "Añadir Gasto"}
+                />
+            </form>
+        </div>
+    )
+}
+
+export default Modal
